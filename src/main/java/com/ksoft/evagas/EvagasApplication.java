b4b53c84a00e2c3072fb0e9fa9301a766074ea95
Francisco Leonardo Leite Oliveira <francisco.leonardo11@outlook.com>
2025-03-18 20:21:12 -0300

    first message

---
+++ src/main/java/com/ksoft/evagas/EvagasApplication.java
@@ -0,0 +1,13 @@
+package com.ksoft.evagas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvagasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvagasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ksoft/evagas/EvagasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksoft.evagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
